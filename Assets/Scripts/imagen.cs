5828bc0594be56b46cc37f713a913687d86e1b3d
ruben <rubenpardocasanova@gmail.com>
2022-10-31 09:30:59 +0100

    Escena Manual

---
+++ Assets/Scripts/imagen.cs
@@ -0,0 +1,184 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+using TMPro;
+
+public class imagen : MonoBehaviour
+{
+
+    public TextMeshProUGUI  textoSiHayLuz;
+
+    #region PRIVATE_MEMBERS
+
+#if UNITY_EDITOR
+    PixelFormat mPixelFormat = PixelFormat.RGB888; // Editor passes in a RGBA8888 texture instead of RGB888
+#else
+    PixelFormat mPixelFormat = PixelFormat.RGB888; // Use RGB888 for mobile
+#endif
+    private bool mFormatRegistered = false;
+    private Texture2D texture;
+
+    private int width;
+    private int height;
+    #endregion // PRIVATE_MEMBERS
+
+    #region MONOBEHAVIOUR_METHODS
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Register Vuforia life-cycle callbacks:
+        VuforiaApplication.Instance.OnVuforiaStarted += RegisterFormat;
+        VuforiaApplication.Instance.OnVuforiaPaused += OnPause;
+        VuforiaBehaviour.Instance.World.OnStateUpdated += OnVuforiaUpdated;
+    }
+
+
+    void OnDestroy()
+    {
+        // Unregister Vuforia life-cycle callbacks:
+        VuforiaApplication.Instance.OnVuforiaStarted -= RegisterFormat;
+        VuforiaApplication.Instance.OnVuforiaPaused -= OnPause;
+        VuforiaBehaviour.Instance.World.OnStateUpdated -= OnVuforiaUpdated;
+    }
+
+    #endregion // MONOBEHAVIOUR_METHODS
+
+    #region PRIVATE_METHODS
+    /// 
+    /// Called each time the Vuforia state is updated
+    /// 
+    void OnVuforiaUpdated()
+    {
+        if (mFormatRegistered)
+        {
+            texture = new Texture2D(width, height, TextureFormat.RGB24, false);
+            Vuforia.Image image = VuforiaBehaviour.Instance.CameraDevice.GetCameraImage(mPixelFormat);
+            image.CopyBufferToTexture(texture);
+            texture.Apply();
+
+
+            Debug.Log(
+                "\nImage Format: " + image.PixelFormat +
+                "\nImage Size: " + image.Width + " x " + image.Height +
+                "\nBuffer Size: " + image.BufferWidth + " x " + image.BufferHeight +
+                "\nImage Stride: " + image.Stride + "\n"
+                
+            );
+            
+
+            if(isTexturaConLuz(texture)){
+                textoSiHayLuz.text = "HAY LUZ";
+            }else{
+                textoSiHayLuz.text = "NO HAY LUZ";
+            }   
+            
+        }
+    }
+
+
+    ///
+    /// Dada una textura obtiene el color medio y
+    /// devuelve true si tiene suficiente luz (componente V de HSV es mayor que .3)
+    /// (componente V 0 -> negro 1 -> blanco)
+    ///
+    bool isTexturaConLuz(Texture2D tex){
+
+        // obtener color medio
+        Color colorMedio = averageColor(tex);
+
+        
+        // transformar de RGB a HSV
+        float H, S, V; // datos normalizados de 0 a 1, H va de 0 a 360 y la S y la V de 0 a 100
+
+        Color.RGBToHSV(colorMedio, out H, out S, out V);
+        
+        Debug.Log ("H >> "+ H) ;
+        Debug.Log ("S >> "+ S) ;
+        Debug.Log ("V >> "+ V) ;
+
+        return V>=0.3;
+
+    }
+
+    ///
+    /// Obtener el color medio de una textura
+    ///
+    Color  averageColor ( Texture2D tex ) {
+        Color32[] texColors = tex.GetPixels32();
+ 
+        int total = texColors.Length;
+ 
+        float r = 0;
+        float g = 0;
+        float b = 0;
+ 
+        for(int i = 0; i < total; i++)
+        {
+ 
+            r += texColors[i].r;
+ 
+            g += texColors[i].g;
+ 
+            b += texColors[i].b;
+ 
+        }
+
+       
+
+        return new Color32((byte)(r / total) , (byte)(g / total) , (byte)(b / total) , 0);
+       
+    }
+
+    /// 
+    /// Called when app is paused / resumed
+    /// 
+    void OnPause(bool paused)
+    {
+        if (paused)
+        {
+            Debug.Log("App was paused");
+            UnregisterFormat();
+        }
+        else
+        {
+            Debug.Log("App was resumed");
+            RegisterFormat();
+        }
+    }
+    /// 
+    /// Register the camera pixel format
+    /// 
+    void RegisterFormat()
+    {
+        // Vuforia has started, now register camera image format
+        bool success = VuforiaBehaviour.Instance.CameraDevice.SetFrameFormat(mPixelFormat, true);
+        if (success)
+
+        {
+            Debug.Log("Successfully registered pixel format " + mPixelFormat.ToString());
+            mFormatRegistered = true;
+        }
+        else
+        {
+            Debug.LogError(
+                "Failed to register pixel format " + mPixelFormat.ToString() +
+                "\n the format may be unsupported by your device;" +
+                "\n consider using a different pixel format.");
+            mFormatRegistered = false;
+        }
+    }
+    /// 
+    /// Unregister the camera pixel format (e.g. call this when app is paused)
+    /// 
+    void UnregisterFormat()
+    {
+        Debug.Log("Unregistering camera pixel format " + mPixelFormat.ToString());
+        VuforiaBehaviour.Instance.CameraDevice.SetFrameFormat(mPixelFormat, false);
+        mFormatRegistered = false;
+    }
+    #endregion //PRIVATE_METHODS
+}
---
+++ Assets/Scripts/manipular.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class manipular : MonoBehaviour
+{
+    public float rotSpeed = 0.4f;
+    private float initialDistance;
+    private Vector3 initialScale;
+    
+
+    // atributos para resetear la poscion
+    public Button btn_resetear;
+    private Vector3 escalaInicial = new Vector3(0.7883818f,0.008797552f,1.045946f);
+    private Vector3 posicionInicial =  new Vector3(0.0f,0.02f,0.0f);
+    private Vector3 angulosInicial =  Vector3.zero;
+
+    void resetear_posicion_manual(){
+        Debug.Log("RESETEAR");
+        this.transform.localPosition = posicionInicial;
+        this.transform.localEulerAngles  = angulosInicial;
+        this.transform.localScale  = escalaInicial;
+    }
+
+    void Start(){
+        
+        // inicializar la posicion original
+        btn_resetear.onClick.AddListener(resetear_posicion_manual);
+    }
+
+    void Update()
+    {
+        if (objIsTouched())
+        {
+            if (Input.touchCount == 1)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Moved)
+                {
+                    Quaternion initRotation = this.transform.rotation;
+                    this.transform.Rotate(Input.GetTouch(0).deltaPosition.y * rotSpeed, -Input.GetTouch(0).deltaPosition.x * rotSpeed, 0, Space.World);
+                }
+            }
+
+            if (Input.touchCount == 2)
+            {
+                var touchZero = Input.GetTouch(0);
+                var touchOne = Input.GetTouch(1);
+
+                if (touchZero.phase == TouchPhase.Ended || touchZero.phase == TouchPhase.Canceled
+                   || touchOne.phase == TouchPhase.Ended || touchOne.phase == TouchPhase.Canceled)
+                {
+                    return;
+                }
+
+                if (touchZero.phase == TouchPhase.Began || touchOne.phase == TouchPhase.Began)
+                {
+                    initialDistance = Vector2.Distance(touchZero.position, touchOne.position);
+                    initialScale = this.transform.localScale;
+                }
+                else
+                {
+                    var currentDistance = Vector2.Distance(touchZero.position, touchOne.position);
+                    if (Mathf.Approximately(initialDistance, 0)) return;
+                    var factor = currentDistance / initialDistance;
+                    this.transform.localScale = initialScale * factor;
+                }
+            }
+        } 
+    }
+
+    private bool objIsTouched()
+    {
+        foreach(Touch t in Input .touches)
+        {
+            Ray m_ray = Camera.main.ScreenPointToRay(t.position);
+            RaycastHit m_hit;
+            if (Physics.Raycast(m_ray, out m_hit, 100))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
