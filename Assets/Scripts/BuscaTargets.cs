dcf8ac8161c604967fb876f84caa64bc81a3bb0f
Jorge <jgragia@epsg.upv.es>
2022-11-07 13:50:22 +0100

    BuscarTarjeta y cambios en la escena del vehiculo

---
+++ Assets/Scripts/BuscaTargets.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuscaTargets : MonoBehaviour
+{
+
+    public GameObject canvasTarjeta;
+    public GameObject buscaTarjeta;
+    // Update is called once per frame
+    void Update()
+    {
+        if (canvasTarjeta.GetComponent<Canvas>().enabled)
+        {
+            if (buscaTarjeta.activeInHierarchy)
+            {
+                buscaTarjeta.SetActive(false);
+            }
+
+            Debug.Log("Activa");
+
+        }
+        else
+        {
+            if (!buscaTarjeta.activeInHierarchy)
+            {
+                buscaTarjeta.SetActive(true);
+            }
+
+            Debug.Log("Desactivada");
+        }
+    }
+}
